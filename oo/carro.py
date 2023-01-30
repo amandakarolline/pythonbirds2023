36feb57d9c26328744eca7ffb90e8ac6c43609ae
amanda <amandakarolline@hotmail.com>
2023-01-29 23:44:48 -0300

    Implementado classe Carro.

--- oo/carro.py
+++ oo/carro.py
@@ -71,30 +71,31 @@ O   L
   >>> carro.calcular_velocidade()
   0
   >>> carro.acelerar()
-  >>> carro.calcular_celocidade()
+  >>> carro.calcular_velocidade()
   1
   >>> carro.acelerar()
-  >>> carro.calcular_celocidade()
+  >>> carro.calcular_velocidade()
   2
   >>> carro.frear()
-  >>> carro.calcular_celocidade()
+  >>> carro.calcular_velocidade()
   0
   >>> carro.calcular_direcao()
-  >>> 'Norte'
+  'Norte'
   >>> carro.girar_a_direita()
   >>> carro.calcular_direcao()
-  >>> 'Leste'
+  'Leste'
   >>> carro.girar_a_esquerda()
   >>> carro.calcular_direcao()
-  >>> 'Norte'
+  'Norte'
   >>> carro.girar_a_esquerda()
   >>> carro.calcular_direcao()
-  >>> 'Oeste'
+  'Oeste'
 """
-NORTE='Norte'
-SUL='Sul'
-LESTE='Leste'
-OESTE='Oeste'
+NORTE = 'Norte'
+SUL = 'Sul'
+LESTE = 'Leste'
+OESTE = 'Oeste'
+
 
 class Motor:
     def __init__(self):
@@ -111,6 +112,7 @@ class Motor:
 class Direcao:
     rotacao_a_direita_dct = {NORTE: LESTE, LESTE: SUL, SUL: OESTE, OESTE: NORTE}
     rotacao_a_esquerda_dct = {NORTE: OESTE, OESTE: SUL, SUL: LESTE, LESTE: NORTE}
+
     def __init__(self):
         self.valor = NORTE
 
@@ -120,3 +122,28 @@ class Direcao:
     def girar_a_esquerda(self):
         self.valor = self.rotacao_a_esquerda_dct[self.valor]
 
+
+class Carro:
+    def __init__(self, direcao, motor):
+        self.motor = motor
+        self.direcao = direcao
+
+    def calcular_velocidade(self):
+        return self.motor.velocidade
+
+    def acelerar(self):
+        return self.motor.acelerar()
+
+    def frear(self):
+        return self.motor.frear()
+
+    def calcular_direcao(self):
+        return self.direcao.valor
+
+    def girar_a_direita(self):
+        return self.direcao.girar_a_direita()
+
+    def girar_a_esquerda(self):
+        return self.direcao.girar_a_esquerda()
+
+        
